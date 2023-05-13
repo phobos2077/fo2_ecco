ba4c7797ce28def104c8cebb3729661ff4ada14c
phobos2077 <phobos2077@gmail.com>
2023-05-13 15:51:54 +0200

    Refactor gecko and rat hunger scripts

--- scripts_src/_pbs_headers/gecko_hunger.h
+++ scripts_src/_pbs_headers/gecko_hunger.h
@@ -8,6 +8,21 @@
 #include "../headers/itempid.h"
 #include "../headers/animcomd.h"
 
+#ifndef FOOD_DETECT_RADIUS
+   #define FOOD_DETECT_RADIUS (15)
+#endif
+
+#ifndef FOOD_SEEK_IMPROBABILITY
+   #define FOOD_SEEK_IMPROBABILITY (50)
+#endif
+
+#define FOOD_PREFERENCE_GECKO (0)
+#define FOOD_PREFERENCE_RAT (1)
+
+variable food;
+
+
+
 procedure getNearestFood(variable maxRadius, variable foodList) begin
    variable items, item, dist, minDist, food;
    items := list_as_array(LIST_GROUNDITEMS);
@@ -23,18 +38,6 @@ procedure getNearestFood(variable maxRadius, variable foodList) begin
       end
    end
    return food;
-end
-
-procedure getNearestFoodForGecko(variable maxRadius) begin
-   variable foodList;
-   foodList := [PID_MEAT_JERKY, PID_IGUANA_ON_A_STICK, PID_MEAT_ON_A_STICK];
-   return getNearestFood(maxRadius, foodList);
-end
-
-procedure getNearestFoodForRat(variable maxRadius) begin
-   variable foodList;
-   foodList := [PID_CHEEZY_POOFS, -1];
-   return getNearestFood(maxRadius, foodList);
 end
 
 procedure anim_go_to(variable food) begin
@@ -53,5 +56,34 @@ procedure anim_go_eat_food(variable food) begin
 end
 
 #define should_delete_food(food)    (not(anim_busy(self_obj)) and tile_distance_objs(self_obj, food) <= 2)
+
+procedure getFoodPidList(variable preference) begin
+   switch (preference) begin
+      case FOOD_PREFERENCE_GECKO:
+         return [PID_MEAT_JERKY, PID_IGUANA_ON_A_STICK, PID_MEAT_ON_A_STICK];
+      case FOOD_PREFERENCE_RAT:
+         return [PID_CHEEZY_POOFS, PID_MUTATED_FRUIT];
+   end
+end
+
+procedure seekAndEatFood(variable improbability, variable radius, variable foodPreference) begin
+   if (random(0, improbability) == 0 and not(food)) then begin
+      food := getNearestFood(radius, getFoodPidList(foodPreference));
+      if (food) then begin
+         call anim_go_to(food);
+      end
+   end else if (food) then if (should_delete_food(food)) then begin
+      use_obj(food);
+      destroy_object(food);
+      food := 0;
+   end
+end
+
+procedure forgetFood begin
+   food := 0; // forget about food in combat
+end
+
+#define SEEK_AND_EAT_FOOD_GECKO  call seekAndEatFood(15, 50, FOOD_PREFERENCE_GECKO);
+#define SEEK_AND_EAT_FOOD_RAT  call seekAndEatFood(15, 50, FOOD_PREFERENCE_RAT);
 
 #endif
