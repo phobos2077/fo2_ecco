a87be261ee1645c88b9748eeac92b101659013cb
phobos2077 <phobos2077@gmail.com>
2023-06-07 01:53:25 +0200

    Dump critter and item stats to text

--- proto_src/dump_critters.cmd
+++ proto_src/dump_critters.cmd
@@ -4,13 +4,14 @@ if not exist "critters" mkdir critters
 SET out_path=%cd%\critters
 SET script_path=%cd%\harold_proto.py
 
-goto vanilla
+rem goto vanilla
 
 cd ..\root\data\proto\critters
 for /r %%i in (*.pro) do (
     "%script_path%" "%%~fi" > %out_path%\%%~ni.json
 )
 
+goto end
 
 rem cd /d D:\GAMES\!Arhives\Fallout\VANILLA\proto\CRITTERS\
 
@@ -21,3 +22,5 @@ cd /d C:\Projects\fo2_rp\data\proto\critters\
 for /r %%i in (*.pro) do (
     "%script_path%" "%%~fi" > ..\critters_json\%%~ni.json
 )
+
+:end
