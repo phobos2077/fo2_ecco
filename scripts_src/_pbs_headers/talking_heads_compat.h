51f7033647771685d1415866bcd94e62b3184991
phobos2077 <phobos2077@gmail.com>
2023-06-12 02:45:20 +0200

    Fix Talking Heads compatibility and recompile all scripts

--- scripts_src/_pbs_headers/talking_heads_compat.h
+++ scripts_src/_pbs_headers/talking_heads_compat.h
@@ -1,13 +1,15 @@
 #ifndef H_TALKING_HEADS_COMPAT
 #define H_TALKING_HEADS_COMPAT
 
-#define talking_head_art_fid(headId)   (0x8000000 bwor (headId bwand 0xFFFF))
+#define talking_head_art_fid(headId)   (0x80A0000 bwor (headId bwand 0xFFF))
 #define talking_head_exists(headId)    art_exists(talking_head_art_fid(headId))
 #define start_gdialog_check_talking_head(reactLvl, headId, bgId) \
    if (talking_head_exists(headId)) then \
       start_gdialog(NAME, self_obj, reactLvl, headId, bgId); \
    else \
       start_gdialog(NAME, self_obj, reactLvl, -1, -1)
+
+// #define debug_talking_head_art(headId)    display_msg(sprintf("Art: %x", talking_head_art_fid(headId))+", exist:"+art_exists(talking_head_art_fid(headId)));
 
 #define start_dialog_at_node_check_talking_head(x, headId, bgId)  \
       if (in_dialog == false) then begin           \
