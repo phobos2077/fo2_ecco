f0e8c31a3f1bea3365cfd5230840232b3e768023
phobos2077 <phobos2077@gmail.com>
2023-07-26 01:49:34 +0200

    Steal mechanic tweaks
    - Changed catch modifier from target skill-based to perception based
    - Only apply catch modifier if steal roll was a failure
    - Apply target skill to initial steal roll, to make it impossible to steal below 20% skill from most NPCs

--- scripts_src/_pbs_headers/ecco_log.h
+++ scripts_src/_pbs_headers/ecco_log.h
@@ -3,6 +3,7 @@
 
 #define debug_log(msg)              debug_msg(SCRIPT_REALNAME": "+msg)
 #define debug_log_fmt(msg, ...)     debug_msg(SCRIPT_REALNAME": "+string_format(msg, __VA_ARGS__))
+#define debug_log_fmt_arr(msg, arr) debug_msg(SCRIPT_REALNAME": "+sprintf_array(msg, arr))
 #define debug_err(msg)              debug_msg("! ERROR ! "SCRIPT_REALNAME": "+msg)
 #define debug_err_fmt(msg, ...)     debug_msg("! ERROR ! "SCRIPT_REALNAME": "+string_format(msg, __VA_ARGS__))
 
