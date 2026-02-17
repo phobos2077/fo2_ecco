3a775e8fb1b9826fcc51f33b74069529b5064ba3
phobos2077 <phobos2077@gmail.com>
2026-02-18 00:15:33 +0100

    Expanded doctor's services to include healing party and limbs: Dr John, Dr Fung, Dr Jubilee
    - Jubilee heals everyone and price scales with number of people healed
    - Dr John heals limbs (+ some logic fixes similar to RPU v35)
    - Dr Fung heals party; price and reply message changes with number and severity of injuries

---
+++ scripts_src/_pbs_headers/healers.h
@@ -0,0 +1,29 @@
+#ifndef PBS_HEALERS_H
+#define PBS_HEALERS_H
+
+#include "party.h"
+
+procedure party_injured_or_crippled_count(variable includeDude := false) begin
+   variable count = 0;
+   foreach (variable who in party_member_list_critters) begin
+      if (who == dude_obj and not includeDude) then continue;
+      if Is_Injured(who) or Is_Crippled(who) then count += 1;
+   end
+   return count;
+end
+
+procedure heal_and_uncripple_critter(variable who) begin
+   critter_heal(who, (get_max_hits(who) - get_cur_hits(who)));
+   critter_uninjure(who, (DAM_CRIP_LEG_LEFT bwor DAM_CRIP_LEG_RIGHT bwor DAM_CRIP_ARM_LEFT bwor DAM_CRIP_ARM_RIGHT));
+end
+
+procedure heal_and_uncripple_party(variable includeDude := false) begin
+   foreach (variable who in party_member_list_critters) begin
+      if (who == dude_obj and not includeDude) then continue;
+
+      call heal_and_uncripple_critter(who);
+   end
+end
+
+
+#endif
