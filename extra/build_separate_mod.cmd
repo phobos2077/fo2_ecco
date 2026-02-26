f41b33fc8181b5b24cd820fdbda875abfdd46635
phobos2077 <phobos2077@gmail.com>
2026-02-26 15:12:15 +0100

    Weapon Sound Variations mod: add readme, AI thumbnail, new folder structure, zip with loose files instead of dat

--- extra/build_separate_mod.cmd
+++ extra/build_separate_mod.cmd
@@ -10,8 +10,9 @@ SET root=%cd%
 
 cd /d "%cwd%"
 
-echo Mod Name = %1
-if "%1"=="weapon_sfx" goto weapon_sfx
+set modName=%1
+echo Mod Name = %modName%
+if "%modName%"=="weapon_sfx" goto weapon_sfx
 
 echo Mod '%1' doesn't exist!
 goto quit_with_error
@@ -26,20 +27,20 @@ echo Compiling weapon sfx script...
 SET scriptName=gl_weapon_sfx_variation
 
 cd /d "%root%\scripts_src\_pbs_main"
-SET scriptOutPath=%root%\separate_mods\%modFolder%\scripts\%scriptName%.int
+SET scriptOutPath=%root%\separate_mods\%modName%\mods\%modFolder%\scripts\%scriptName%.int
 
 call compile_wcc "%scriptName%.ssl" -o "%scriptOutPath%"
 set compileErr=%errorlevel%
 cd /d "%cwd%"
 if %compileErr% geq 1 goto quit_with_error
 
-goto make_dat
-
+goto make_zip_no_dat
+rem goto make_dat
 
 
 :make_dat
 
-echo Building DAT for mod '%modFolder%'...
+echo Building DAT for mod '%modName%'...
 
 SET datFile=fo2_%modFolder%.dat
 SET datFilePath=%root%\separate_mods\%datFile%
@@ -49,10 +50,34 @@ if exist %datFilePath% (
    del %datFilePath%
 )
 
-cd /d "%root%\separate_mods\%modFolder%"
+cd /d "%root%\separate_mods\%modName%\mods\%modFolder%"
 rem echo CD=%cd%
 %root%\extra\bin\dat2.exe a -r %datFile% %datInputList%
 move %datFile% %root%\separate_mods\
+
+cd /d "%cwd%"
+echo Done!
+
+exit /B
+
+
+:make_zip_no_dat
+
+echo Building ZIP for mod '%modName%'...
+
+SET zipFile=fo2_%modFolder%.zip
+SET zipFilePath=%root%\separate_mods\%zipFile%
+
+if exist %zipFilePath% (
+   echo Deleting existing %zipFilePath%...
+   del %zipFilePath%
+)
+
+cd /d "%root%\separate_mods\%modName%"
+
+echo Running 7Zip...
+set exe="C:\Program Files\7-Zip\7z.exe"
+%exe% a %zipFilePath% mods\* sfall\* *.txt > nul
 
 cd /d "%cwd%"
 echo Done!
---
+++ separate_mods/weapon_sfx/readme_weapon_sfx_variation.txt
@@ -0,0 +1,26 @@
+Unlocks some of the originally unused variations of weapon sound effects.
+
+Requires sfall 4.4.5 or higher.
+
+Should be compatible with RPU, UPU, EtTu, as well as vanilla game.
+
+
+Installation:
+- Extract contents into game root folder.
+- Verify that you have "scripts" folder inside "<GameRoot>\mods\weapon_sfx_variation\".
+- Open your "<GameRoot>\mods\mods_order.txt" and add "weapon_sfx_variation" (without quotes) to the end of the file.
+
+
+
+Credits:
+- phobos2077: sfall hook, script, ini file
+- NovaRain: testing
+
+
+Remarks:
+
+Originally a part of EcCo, now a standalone mod.
+If you liked this mod, please consider the full EcCo package:
+
+https://www.moddb.com/mods/ecco-gameplay-overhaul-for-restoration-project
+https://github.com/phobos2077/fo2_ecco/releases
